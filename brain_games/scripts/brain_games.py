8d2951b60b25d5c28491751f03e37d81729948b0
Eugene Kvach <euhoo@mail.ru>
2023-11-19 20:14:52 +0300

    step3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+import prompt
+
+def print_bg():
+    print('brain-games')
+
 def print_hello():
     print('Welcome to the Brain Games!')
-    
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name)
+
 def main():
+    print_bg()
     print_hello()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
